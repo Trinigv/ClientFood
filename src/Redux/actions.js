d4de34ceb222efcfbb2befc551408bb7f2969fad
Trinigv <99trini@gmail.com>
2023-03-21 13:29:21 -0300

    New Build

--- src/Redux/actions.js
+++ src/Redux/actions.js
@@ -8,9 +8,6 @@ export const SORT_HEALTHSCORE = 'SORT_HEALTHSCORE';
 export const ALPHABET = 'ALPHABET';
 export const GET_RECIPE_NAME = 'GET_RECIPE_NAME';
 require('dotenv').config();
-{
-	/*https://foodapi-l1xc.onrender.com*/
-}
 
 export const getBackendRecipes = () => {
 	return async function ask(dispatch) {
